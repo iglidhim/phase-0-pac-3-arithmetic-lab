a420b8dcb35f5ba82f5510b9053653d3a4305839
Igli Dhima <iglidhim@amazon.com>
2021-03-13 21:31:11 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a) {
+return a++;
+}
+function subtract(a) {
+return a--;
+}
+function multiply(a) {
+return a *= 2;
+}
+function divide(a) {
+    return a /= 2;
+}
+function add(a,b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n+= 1;
+}
+function decrement(n) {
+    return n-= 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
